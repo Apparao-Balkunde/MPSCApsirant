41574523d523187a530a2e77d2eb5941d2f529da
APPARAO BALKUNDE <apparaobalkunde901@gmail.com>
2026-09-18 19:03:35 +0530

    feat(data): add questions set 5 to extra questions collection
    Integrate the new question set into the master list of 100 extra practice questions for improved coverage.

--- src/data/extraQuestions100.ts
+++ src/data/extraQuestions100.ts
@@ -1,8 +1,11 @@
 import { Question } from '../types';
 import { QUESTIONS_SET_3 } from './questionsSet3';
 import { QUESTIONS_SET_4 } from './questionsSet4';
+import { QUESTIONS_SET_5 } from './questionsSet5';
 
 export const EXTRA_QUESTIONS_100: Question[] = [
   ...QUESTIONS_SET_3,
   ...QUESTIONS_SET_4,
+  ...QUESTIONS_SET_5,
 ];
+
