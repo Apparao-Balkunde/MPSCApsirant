0dbfff798316e12a0c29820b676d1b16106ed4f6
APPARAO BALKUNDE <apparaobalkunde901@gmail.com>
2026-09-18 19:05:30 +0530

    feat(data): add questions set 6 to extra questions
    Include 477 new questions from the sixth set in the extra questions collection to expand the practice database.

--- src/data/extraQuestions100.ts
+++ src/data/extraQuestions100.ts
@@ -2,10 +2,13 @@ import { Question } from '../types';
 import { QUESTIONS_SET_3 } from './questionsSet3';
 import { QUESTIONS_SET_4 } from './questionsSet4';
 import { QUESTIONS_SET_5 } from './questionsSet5';
+import { QUESTIONS_SET_6 } from './questionsSet6';
 
 export const EXTRA_QUESTIONS_100: Question[] = [
   ...QUESTIONS_SET_3,
   ...QUESTIONS_SET_4,
   ...QUESTIONS_SET_5,
+  ...QUESTIONS_SET_6,
 ];
+
 
