34393db5e5d14c0c097c64b537c6b70f9c98602e
APPARAO BALKUNDE <apparaobalkunde901@gmail.com>
2026-09-18 19:09:40 +0530

    feat(data): add questions set 8 to extra questions collection
    Include the new QUESTIONS_SET_8 in the main data export for the 100-question extra practice module.

--- src/data/extraQuestions100.ts
+++ src/data/extraQuestions100.ts
@@ -4,6 +4,7 @@ import { QUESTIONS_SET_4 } from './questionsSet4';
 import { QUESTIONS_SET_5 } from './questionsSet5';
 import { QUESTIONS_SET_6 } from './questionsSet6';
 import { QUESTIONS_SET_7 } from './questionsSet7';
+import { QUESTIONS_SET_8 } from './questionsSet8';
 
 export const EXTRA_QUESTIONS_100: Question[] = [
   ...QUESTIONS_SET_3,
@@ -11,7 +12,9 @@ export const EXTRA_QUESTIONS_100: Question[] = [
   ...QUESTIONS_SET_5,
   ...QUESTIONS_SET_6,
   ...QUESTIONS_SET_7,
+  ...QUESTIONS_SET_8,
 ];
+
 
 
 
