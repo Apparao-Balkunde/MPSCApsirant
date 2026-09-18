af9f046f8ec66c6ae7ccd1c6003914a4f04d5e6d
APPARAO BALKUNDE <apparaobalkunde901@gmail.com>
2026-09-18 19:10:47 +0530

    feat(data): add questions set 9
    Incorporate a new set of questions focused on Maharashtra social reformers and women pioneers into the EXTRA_QUESTIONS_100 collection.

--- src/data/extraQuestions100.ts
+++ src/data/extraQuestions100.ts
@@ -5,6 +5,7 @@ import { QUESTIONS_SET_5 } from './questionsSet5';
 import { QUESTIONS_SET_6 } from './questionsSet6';
 import { QUESTIONS_SET_7 } from './questionsSet7';
 import { QUESTIONS_SET_8 } from './questionsSet8';
+import { QUESTIONS_SET_9 } from './questionsSet9';
 
 export const EXTRA_QUESTIONS_100: Question[] = [
   ...QUESTIONS_SET_3,
@@ -13,7 +14,9 @@ export const EXTRA_QUESTIONS_100: Question[] = [
   ...QUESTIONS_SET_6,
   ...QUESTIONS_SET_7,
   ...QUESTIONS_SET_8,
+  ...QUESTIONS_SET_9,
 ];
+
 
 
 
