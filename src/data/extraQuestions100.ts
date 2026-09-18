c5734754c9d43421b82b6623ff902c7472a204c0
APPARAO BALKUNDE <apparaobalkunde901@gmail.com>
2026-09-18 19:07:48 +0530

    feat: add questionsSet7 to extra questions dataset
    Include the newly created QUESTIONS_SET_7, focusing on Panchayati Raj and local self-government, into the consolidated EXTRA_QUESTIONS_100 list.

--- src/data/extraQuestions100.ts
+++ src/data/extraQuestions100.ts
@@ -3,12 +3,15 @@ import { QUESTIONS_SET_3 } from './questionsSet3';
 import { QUESTIONS_SET_4 } from './questionsSet4';
 import { QUESTIONS_SET_5 } from './questionsSet5';
 import { QUESTIONS_SET_6 } from './questionsSet6';
+import { QUESTIONS_SET_7 } from './questionsSet7';
 
 export const EXTRA_QUESTIONS_100: Question[] = [
   ...QUESTIONS_SET_3,
   ...QUESTIONS_SET_4,
   ...QUESTIONS_SET_5,
   ...QUESTIONS_SET_6,
+  ...QUESTIONS_SET_7,
 ];
+
 
 
